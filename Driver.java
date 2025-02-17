b16bd32a347d2e031167eddd9dc48c583df18aae
ta422 <tatianaescobar889@gmail.com>
2025-02-17 11:57:06 -0600

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,7 @@
+class Driver(String mame, String document, String email, String password){
+    super(name, document, email, password);
+}
+void printDataDriver(){
+    System.out.println("Document driver: " + document + " Name 
+    driver:" + name + "E-mail:" + email + "password:" password);
+}
